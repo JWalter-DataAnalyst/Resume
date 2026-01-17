3700dbd9207721dfd5c1d8c59a2f49348f7fc3c7
JWalter-DataAnalyst <jimmywalters11@yahoo.com>
2026-01-17 22:33:14 +0000

    Deploying to gh-pages from @ JWalter-DataAnalyst/Resume@076baec9f649c5a0cc3a05097848cbebe057f4c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,9 +402,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-recession-indicator-s",
+          title: 'Recession Indicator(s)',
+          description: "An emperical report I did in order to determine whether or not we will face an economic recession or not",
           section: "Projects",handler: () => {
               window.location.href = "/Resume/projects/1_project/";
             },},{id: "projects-project-2",
